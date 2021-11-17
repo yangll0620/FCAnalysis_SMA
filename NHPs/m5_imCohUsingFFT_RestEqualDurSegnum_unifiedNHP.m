251917ae7fe2849cd166d6e2b81a904ed890cdd9
LRB411-Win10 <yll0620@gmail.com>
2021-11-17 12:24:31 -0600

    m5_imCoh_.._RestEqual_..unified

---
+++ NHPs/m5_imCohUsingFFT_RestEqualDurSegnum_unifiedNHP.m
@@ -0,0 +1,182 @@
+function m5_imCohUsingFFT_RestEqualDurSegnum_unifiedNHP(varargin)
+if nargin < 1
+    animal = 'Jo';
+else
+    animal = varargin{1};
+end
+
+codefilepath = mfilename('fullpath');
+
+% find the codefolder
+idx = strfind(codefilepath, 'code');
+codefolder = codefilepath(1:idx + length('code')-1);
+clear idx
+
+
+% add path
+addpath(genpath(fullfile(codefolder,'util')));
+addpath(genpath(fullfile(codefolder,'NHPs')));
+addpath(genpath(fullfile(codefolder,'connAnalyTool')));
+
+% find animal corresponding folder
+[~, codefilename]= fileparts(codefilepath);
+
+NHPCodefilepath = fullfile(codefolder, 'NHPs', animal, '0_dataPrep' , 'SKT', codefilename);
+[codecorresfolder, ~] = code_corresfolder(NHPCodefilepath, true, false);
+
+
+%% save setup
+savefolder = codecorresfolder;
+copyfile2folder(codefilepath, savefolder);
+
+ciCohPhasefile_prefix =[animal '_Rest_ciCohPhasefile'];
+
+%%  input setup
+
+% inputfolder_Rest & inputcodefolder_SKT
+inputcodefolder_Rest = fullfile(codefolder, 'NHPs', animal, '0_dataPrep' , 'Rest', 'm3_restData_rmChns_avgArea');
+[inputfolder_Rest, ~] = code_corresfolder(inputcodefolder_Rest, false, false);
+
+SKTSubfolder = 'SKT';
+if strcmpi(animal, 'Kitty')
+    SKTSubfolder = 'SKT_SegV';
+end
+inputcodefolder_SKT = fullfile(codefolder, 'NHPs', animal, '0_dataPrep' , SKTSubfolder, 'm4_imCohUsingFFT_EventPhase_unifiedNHP');
+[inputfolder_SKT, ~] = code_corresfolder(inputcodefolder_SKT, false, false);
+clear inputcodefolder_Rest SKTSubfolder inputcodefolder_SKT
+
+twin = 0.2;
+
+image_type = 'tif';
+
+f_AOI = [8 40];
+
+
+shuffleN_psedoTest = 500;
+
+
+%% Code start here
+cond_cell = cond_cell_extract(animal);
+
+for ci = 1 : length(cond_cell)
+    pdcond = cond_cell{ci};
+    
+    
+    % load(and extract) ciCohPhasefile
+    ciCohPhasefile = fullfile(savefolder, [ciCohPhasefile_prefix  '_' pdcond  '.mat']);
+    if(~exist(ciCohPhasefile, 'file'))
+        
+        files = dir(fullfile(inputfolder_Rest, ['*_' pdcond '_*.mat']));
+        [lfpsegs, fs, T_chnsarea]= seg2ShortSegments(files, twin);
+        
+        % match the trial number in rest and SKT
+        load(fullfile(inputfolder_SKT, [animal ' ciCohPhasefile_' pdcond '_earlyReach_align2ReachOnset.mat']), 'ntrials')
+        nsegs = size(lfpsegs, 3);
+        randomSKTInds =  randsample(nsegs,ntrials);
+        lfpsegs = lfpsegs(:, :, randomSKTInds);
+        clear ntrials nsegs randomSKTInds
+        
+        
+        
+        %  extract and save deltaphis_allChnsTrials and cicoh
+        [deltaphis_allChnsSegs, ciCoh, f_selected]= ciCoh_trialDeltaPhi(lfpsegs, fs, f_AOI);
+        
+        % save
+        nsegs = size(lfpsegs, 3);
+        save(ciCohPhasefile, 'deltaphis_allChnsSegs', 'ciCoh', 'T_chnsarea', 'nsegs', 'f_selected');
+            
+        %  extract and save psedociCohs
+        psedociCoh_extract_save(shuffleN_psedoTest, lfpsegs, fs, f_AOI, ciCohPhasefile);
+        
+        clear files lfpsegs fs T_chnsarea
+        clear('deltaphis_allChnsSegs', 'ciCoh', 'T_chnsarea', 'nsegs', 'f_selected');
+    end
+    load(ciCohPhasefile, 'deltaphis_allChnsSegs', 'ciCoh', 'T_chnsarea', 'nsegs', 'f_selected', 'psedociCohs');
+    if ~exist('psedociCohs','var') || size(psedociCohs, 4) < shuffleN_psedoTest
+        files = dir(fullfile(inputfolder_Rest, ['*_' pdcond '_*.mat']));
+        [lfpsegs, fs, T_chnsarea]= seg2ShortSegments(files, twin);
+
+        psedociCoh_extract_save(shuffleN_psedoTest, lfpsegs, fs, f_AOI, ciCohPhasefile);
+        load(ciCohPhasefile, 'psedociCohs');
+        clear t_minmax_reach lfpsegs
+    end
+    nshuffle = size(psedociCohs, 4);
+    
+    % extract sigciCoh
+    [sigciCoh]= sigciCoh_extract(psedociCohs, ciCoh);
+    
+    
+    % extract ciCoh_flatten and chnPairNames, such as M1-stn0-1
+    [sigciCoh_flatten, deltaphis_flatten, chnPairNames] = ciCohDephiFlatten_chnPairNames_extract(sigciCoh, deltaphis_allChnsSegs, T_chnsarea);
+    [ciCoh_flatten_used, deltaphis_flatten_used, chnPairNames_used]= ciCoh_deltaPhi_Used(chnPairNames, sigciCoh_flatten, deltaphis_flatten, removed_chns);
+    
+    
+    % plot and save ciCoh Histogram image
+    titlename = [animal ' Rest-'  pdcond ', nshuffle= ' num2str(nshuffle)];
+    plot_ciCohHistogram(ciCoh_flatten_used, chnPairNames_used, f_selected, titlename);
+    saveimgname = [animal '_' event '_' pdcond '_align2' char(align2) '.' image_type];
+    saveas(gcf, fullfile(savefolder, saveimgname), image_type);
+    clear titlename  saveimgname
+    
+    
+    clear pdcond
+    clear ciCohPhasefile
+end
+
+
+
+
+function [lfpdata, fs, T_chnsarea]= seg2ShortSegments(files, twin)
+if isempty(files)
+    disp('files for seg2ShortSegments empty!')
+    
+    lfpdata = [];
+    fs = [];
+    T_chnsarea = [];
+    
+    return;
+end
+
+lfpdata = [];
+for fi = 1: length(files)
+    loadfilename = files(fi).name;
+    load(fullfile(files(fi).folder, loadfilename), 'data_segments', 'fs', 'T_chnsarea');
+    
+    nwin = round(twin * fs);
+    for segi = 1 : length(data_segments)
+        seglfp = data_segments(segi).lfp;
+        
+        len = size(seglfp, 2);
+        shortSegn = floor(len / nwin);
+        for shortsegi = 1 : shortSegn
+            stri = (shortsegi - 1)* nwin + 1;
+            endi = shortsegi * nwin;
+            lfpdata = cat(3, lfpdata, seglfp(:, stri : endi));
+            clear stri endi
+        end
+        clear seglfp len shortSegn shortsegi
+    end
+    
+    if ~exist('fs_unit', 'var')
+        fs_unit = fs;
+    else
+        if(fs_unit ~=fs)
+            dis(['fs_unit ~=fs for file ' loadfilename])
+        end
+    end
+    
+    if ~exist('T_chnsarea_unit', 'var')
+        T_chnsarea_unit = T_chnsarea;
+    else
+        if(~isequal(T_chnsarea_unit, T_chnsarea))
+            dis(['T_chnsarea_unit ~= T_chnsarea for file ' loadfilename])
+        end
+    end
+    
+    clear nwin segi
+    clear('data_segments', 'fs', 'T_chnsarea')
+    clear loadfilename
+    
+end
+fs = fs_unit;
+T_chnsarea = T_chnsarea_unit;
