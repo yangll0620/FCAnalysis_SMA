a94f2bfb0832984c3de1821b23a417098268dc38
yangwingitbash <yll0620@gmail.com>
2022-03-01 15:24:10 -0500

    present two trials very slowly

---
+++ util/BasicClass.m
@@ -0,0 +1,13 @@
+classdef BasicClass
+   properties
+      Value = 1 
+   end
+   methods
+      function r = roundOff(obj)
+         r = round([obj.Value],2);
+      end
+      function r = multiplyBy(obj,n)
+         r = [obj.Value] * n;
+      end
+   end
+end
