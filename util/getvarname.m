8716fc583df448e814771a59f3d3f2028cdf960f
yangwingitbash <yll0620@gmail.com>
2022-03-21 13:36:09 -0400

    added function getvarname for extracting the variable name to string

---
+++ util/getvarname.m
@@ -0,0 +1,9 @@
+function varname = getvarname(var)
+% return the variable name
+%
+%   Example:
+%       varname = getvarname(optFreezeTypes); % varname =  'optFreezeTypes'
+%       
+
+varname = inputname(1);
+end
