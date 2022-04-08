23827ab83033a37d98e6b15c5584a59bf205b090
yangwingitbash <yll0620@gmail.com>
2022-04-08 13:50:57 -0400

    added PeakV to eventPhases

--- NHPs/SKT_eventPhases_extract.m
+++ NHPs/SKT_eventPhases_extract.m
@@ -1,2 +1,2 @@
 function EventPhases = SKT_eventPhases_extract()
-EventPhases = {'preMove'; 'Anticipated';'earlyReach';'lateReach'; 'Return'};
+EventPhases = {'preMove'; 'earlyReach';'lateReach'; 'peakV'};
